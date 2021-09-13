b837a854742e87afbb05b9cda6d385743837a3e9
iqbalimaduddin11 <iqbalimaduddin27@gmail.com>
2021-09-13 18:39:43 +0700

    create pages

---
+++ pages/barang/golongan-barang.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container-fluid p-3">
+    <div class="row">
+      <div class="card bg-white" style="width:30%">
+        <label for="golongan-barang" class="fs-6">Input Golongan Barang</label>
+        <input type="textarea" class="form-control" name="golongan-barang" id="golongan-barang" data-bs-toggle="modal" data-bs-target="#Modal">
+
+        <div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Silahkan Input Golongan Barang Baru</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <input type="text" name="golongan-barang" id="golongan-barang">
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary mt-1 mb-2" style="width:22%;">Input</button>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <table class="table">
+        <thead class="table table-dark table-bordered border-white">
+          <tr>
+            <th>No</th>
+            <th>Golongan</th>
+            <th>aksi</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>1.</th>
+            <th>Elektronik</th>
+            <th>
+              <a href="/inventaris" class="btn btn-primary" role="button" data-bs-toggle="button">Detail</a>
+              <a href="/inventaris" class="btn btn-danger" role="button" data-bs-toggle="button">Detail</a>
+            </th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<style>
+.row{
+  margin-left: 0;
+  margin-right: 0;
+}
+</style>
---
+++ pages/inventaris/data-inventaris.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="container-fluid p-3">
+    <div class="row">
+      <div class="card bg-white" style="width: 30%;">
+        <label for="golongan-barang" class="fs-6">Input Jenis Barang</label>
+        <input type="textarea" class="form-control" name="golongan-barang" id="golongan-barang" data-bs-toggle="modal" data-bs-target="#Modal-jenis">
+
+        <div class="modal fade" id="Modal-jenis" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Input Jenis Barang</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <label for="nama">Nama Barang</label>
+                <input type="textarea" class="form-control" name="nama" id="nama" data-bs-toggle="modal" data-bs-target="#Modal">
+                <label for="golongan" class="fs-6">Golongan Barang</label>
+                <select class="form-select" aria-label="Default select example" name="golongan" id="nama">
+                  <option selected>Pilih Golongan</option>
+                  <option value="1">Elektronik</option>
+                  <option value="2">ATK</option>
+                  <option value="3">Furniture</option>
+                </select>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary mt-2 mb-2" style="width:22%;">Input</button>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <table class="table">
+        <thead class="table table-dark table-bordered border-white">
+          <tr>
+            <th>Kode Inventaris</th>
+            <th>Tanggal Masuk</th>
+            <th>Harga</th>
+            <th>Donatur</th>
+            <th>Ruang</th>
+            <th>Divisi</th>
+            <th>Kepemilikan</th>
+            <th>Kondisi Barang</th>
+            <th>Status Lokasi</th>
+            <th>Dokumen</th>
+            <th>Pencatat</th>
+            <th>aksi</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>1.</th>
+            <th>13-09-2021</th>
+            <th>1000000</th>
+            <th>Ahmad Faizi</th>
+            <th>Direksi</th>
+            <th>CMC</th>
+            <th>sarpras</th>
+            <th>Bagus</th>
+            <th>Gedung Kelas</th>
+            <th></th>
+            <th>Fauzan</th>
+            <th>
+              <a href="/" class="btn btn-danger" role="button" data-bs-toggle="button">Delete</a>
+              <a href="/" class="btn btn-primary" role="button" data-bs-toggle="button">Edit</a>
+            </th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<style>
+.row{
+  margin-left: 0;
+  margin-right: 0;
+}
+</style>
