23fb898223c08f83dd660ed47a5f75277c331aa5
ahmadilahana <ahmadilahana@gmailcom>
2021-09-16 13:48:03 +0700

    update

--- pages/user/ketua-divisi.vue
+++ pages/user/ketua-divisi.vue
@@ -9,83 +9,73 @@
         </nav>
         <h4 class="ml-3"><strong>Data Ketua Divisi</strong></h4>
         <div class="ml-3 mt-4">
-            <button class="btn btn-sm btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#tambahKetuaModal">Tambah Ketua</button>
-            <!-- Modal -->
-            <div class="modal fade" id="tambahKetuaModal" data-bs-keyboard="false" tabindex="-1" aria-labelledby="tambahKetuaModalLabel" aria-hidden="true">
-            <div class="modal-lg modal-dialog">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="tambahKetuaModalLabel">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form action="" method="post">
-                        <div class="mb-3 row">
-                            <label for="inputNip" class="col-sm-2 col-form-label">NIP</label>
-                            <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputNip">
-                            </div>
+            <b-button v-b-modal.modal-1 class="btn btn-sm" variant="primary">Tambah Ketua</b-button>
+
+            <b-modal id="modal-1" size="lg" ref="modal-admin" title="Tambah Admin Ruang">
+                <form action="" method="post" style="margin-bottom: 90px">
+                    <div class="mb-3 row">
+                        <label for="inputNip" class="col-sm-2 col-form-label">NIP</label>
+                        <div class="col-sm-10">
+                        <input type="text" class="form-control" id="inputNip">
                         </div>
-                        <div class="mb-3 row">
-                            <label for="inputName" class="col-sm-2 col-form-label">Nama</label>
-                            <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputName">
-                            </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="inputName" class="col-sm-2 col-form-label">Nama</label>
+                        <div class="col-sm-10">
+                        <input type="text" class="form-control" id="inputName">
                         </div>
-                        <div class="mb-3 row">
-                            <label for="inputName" class="col-sm-2 col-form-label">Jabatan</label>
-                            <div class="col-sm-10">
-                                <b-form-select v-model="selected" :options="jabatan">
-                                <!-- This slot appears above the options from 'options' prop -->
-                                    <template #first>
-                                        <b-form-select-option :value="null" disabled>-- Pilih Jabatan --</b-form-select-option>
-                                    </template>
-                                </b-form-select>
-                            </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="inputName" class="col-sm-2 col-form-label">Jabatan</label>
+                        <div class="col-sm-10">
+                            <b-form-select v-model="selected" :options="jabatan">
+                            <!-- This slot appears above the options from 'options' prop -->
+                                <template #first>
+                                    <b-form-select-option :value="null" disabled>-- Pilih Jabatan --</b-form-select-option>
+                                </template>
+                            </b-form-select>
                         </div>
-                        <div class="mb-3 row">
-                            <label for="inputName" class="col-sm-2 col-form-label">Divisi</label>
-                            <div class="col-sm-10">
-                                <b-form-select v-model="selected" :options="divisi">
-                                <!-- This slot appears above the options from 'options' prop -->
-                                    <template #first>
-                                        <b-form-select-option :value="null" disabled>-- Pilih Divisi --</b-form-select-option>
-                                    </template>
-                                </b-form-select>
-                            </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="inputName" class="col-sm-2 col-form-label">Divisi</label>
+                        <div class="col-sm-10">
+                            <b-form-select v-model="selected" :options="divisi">
+                            <!-- This slot appears above the options from 'options' prop -->
+                                <template #first>
+                                    <b-form-select-option :value="null" disabled>-- Pilih Divisi --</b-form-select-option>
+                                </template>
+                            </b-form-select>
                         </div>
-                        <div class="mb-3 row">
-                            <label for="inputHp" class="col-sm-2 col-form-label">Nomor Hp</label>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control" id="inputHp">
-                            </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="inputHp" class="col-sm-2 col-form-label">Nomor Hp</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputHp">
                         </div>
-                        <div class="mb-3 row">
-                            <label for="inputAlamat" class="col-sm-2 col-form-label">Alamat</label>
-                            <div class="col-sm-10">
-                                <textarea class="form-control" id="inputAlamat" rows="3"></textarea>
-                            </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="inputAlamat" class="col-sm-2 col-form-label">Alamat</label>
+                        <div class="col-sm-10">
+                            <textarea class="form-control" id="inputAlamat" rows="3"></textarea>
                         </div>
-                        <div class="mb-3 row">
-                            <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-                            <div class="col-sm-10">
-                            <input type="email" class="form-control" id="staticEmail">
-                            </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-10">
+                        <input type="email" class="form-control" id="staticEmail">
                         </div>
-                        <div class="mb-3 row">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-                            <div class="col-sm-10">
-                            <input type="password" class="form-control" id="inputPassword">
-                            </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                        <div class="col-sm-10">
+                        <input type="password" class="form-control" id="inputPassword">
                         </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" id="simpan" @click="simpan">Simpan</button>
-                </div>
-                </div>
-            </div>
-            </div>
+                    </div>
+                </form>
+                <template #modal-footer>
+                    <b-button @click="simpan" variant="primary">Simpan</b-button>
+                </template>
+            </b-modal>
         </div>
         <b-table outlined no-border-collapse :fields="header" :items="items" show-empty class="mt-4">
             <template #empty>
