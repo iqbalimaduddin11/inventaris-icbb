b8538e942f0987a5ca04d1bf84547e579c23da27
iqbalimaduddin11 <iqbalimaduddin27@gmail.com>
2021-09-21 13:56:18 +0700

    edit pages

--- pages/inventaris/data-inventaris.vue
+++ pages/inventaris/data-inventaris.vue
@@ -1,123 +1,109 @@
 <template>
   <div class="container-fluid p-3">
+    <nav aria-label="breadcrumb" class="mt-4">
       <ol class="breadcrumb">
         <li class="breadcrumb-item" aria-current="page"><a href="/">Home</a></li>
         <li class="breadcrumb-item" aria-current="page">Manajemen Inventaris</li>
         <li class="breadcrumb-item active" aria-current="page">Data Inventaris</li>
       </ol>
+    </nav>
     <h4 class="ml-3"><strong>Data Inventaris</strong></h4>
     <div class="row">
       <div class="ml-3 mt-4">
-      <button class="btn btn-sm btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#tambahInventarisModal">Tambah Inventaris</button>
+        <div class="ml-3 mt-4">
+          <b-button v-b-modal.modal-1 class="btn btn-sm" variant="primary">Tambah Inventaris</b-button>
 
-        <div class="modal fade" id="tambahInventarisModal" tabindex="-1" aria-labelledby="tambahInventarisModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="tambahInventarisModalLabel">Tambah Data Inventaris</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <b-modal id="modal-1" size='lg' ref="modal-inventaris" title="Tambah Inventaris">
+            <form action="" method="post" style="margin-bottom: 90px">
+              <div class="mb-3 row">
+                  <label for="example-datepicker" class="col-sm-2 col-form-label">Tanggal</label>
+                <div class="col-sm-10">
+                  <b-form-datepicker id="tanggal" v-model="date" class="mb-2"></b-form-datepicker>
+                </div>
               </div>
-              <div class="modal-body">
-                <form action="" method="post">
-                    <div class="mb-3 row">
-                            <label for="example-datepicker" class="col-sm-2 col-form-label">Tanggal</label>
-                          <div class="col-sm-10">
-                            <b-form-datepicker id="tanggal" v-model="date" class="mb-2"></b-form-datepicker>
-                          </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="inputBarang" class="col-sm-2 col-form-label">Barang</label>
-                        <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputBarang" v-model="barang">
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="inputHarga" class="col-sm-2 col-form-label">Harga</label>
-                        <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputHarga" v-model="harga">
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="inputDonatur" class="col-sm-2 col-form-label">Donatur</label>
-                        <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputDonatur" v-model="donatur">
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="inputRuang" class="col-sm-2 col-form-label">Ruang</label>
-                        <div class="col-sm-10">
-                        <select class="form-select" v-model="ruang" aria-label="Default select example" id="inputRuang">
-                          <option selected>Pilih Ruang</option>
-                          <option value="1">Ruang Direksi</option>
-                          <option value="2">Ruang Produksi</option>
-                        </select>
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="inputDivisi" class="col-sm-2 col-form-label">Divisi</label>
-                        <div class="col-sm-10">
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="inputKepemilikan" class="col-sm-2 col-form-label">Kepemilikan</label>
-                        <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputKepemilikan" v-model="kepemilikan">
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="inputKondisi" class="col-sm-2 col-form-label">Kondisi</label>
-                        <div class="col-sm-10">
-                        <select class="form-select" v-model="kondisi" aria-label="Default select example" id="inputKondisi">
-                          <option selected>Pilih Kondisi</option>
-                          <option value="1">Baik</option>
-                          <option value="2">Cukup</option>
-                          <option value="3">Buruk</option>
-                        </select>
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="inputStatus" class="col-sm-2 col-form-label">Status</label>
-                        <div class="col-sm-10">
-                        <select class="form-select" v-model="status" aria-label="Default select example" id="inputStatus">
-                          <option selected>Pilih Status</option>
-                          <option value="1">Elektronik</option>
-                          <option value="2">ATK</option>
-                          <option value="3">Furniture</option>
-                        </select>
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="inputDokumen" class="col-sm-2 col-form-label">Dokumen</label>
-                        <div class="col-sm-10">
-                          <b-form-file v-model="dokumen" id="file-default"></b-form-file>
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="inputPencatatan" class="col-sm-2 col-form-label">Pencatat</label>
-                        <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputPencatatan" v-model="pencatat">
-                        </div>
-                    </div>
-                </form>
+              <div class="mb-3 row">
+                  <label for="inputBarang" class="col-sm-2 col-form-label">Barang</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputBarang" v-model="barang">
+                  </div>
               </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save</button>
+              <div class="mb-3 row">
+                  <label for="inputHarga" class="col-sm-2 col-form-label">Harga</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputHarga" v-model="harga">
+                  </div>
               </div>
-            </div>
-          </div>
+              <div class="mb-3 row">
+                  <label for="inputDonatur" class="col-sm-2 col-form-label">Donatur</label>
+                  <div class="col-sm-10">
+                  <input type="text" class="form-control" id="inputDonatur" v-model="donatur">
+                  </div>
+              </div>
+              <div class="mb-3 row">
+                  <label for="inputDivisi" class="col-sm-2 col-form-label">Divisi</label>
+                  <div class="col-sm-10">
+                      <input type="text" class="form-control" id="inputDivisi" v-model="divisi">
+                  </div>
+              </div>
+              <div class="mb-3 row">
+                  <label for="inputKepemilikan" class="col-sm-2 col-form-label">Kepemilikan</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputKepemilikan" v-model="kepemilikan">
+                  </div>
+              </div>
+              <div class="mb-3 row">
+                  <label for="inputKondisi" class="col-sm-2 col-form-label">Kondisi</label>
+                  <div class="col-sm-10">
+                    <select class="form-select" v-model="kondisi" aria-label="Default select example" id="inputKondisi">
+                      <option selected>Pilih Kondisi</option>
+                      <option value="1">Baik</option>
+                      <option value="2">Cukup</option>
+                      <option value="3">Buruk</option>
+                    </select>
+                  </div>
+              </div>
+              <div class="mb-3 row">
+                  <label for="inputStatus" class="col-sm-2 col-form-label">Status</label>
+                  <div class="col-sm-10">
+                  <select class="form-select" v-model="status" aria-label="Default select example" id="inputStatus">
+                    <option selected>Pilih Status</option>
+                    <option value="1">Elektronik</option>
+                    <option value="2">ATK</option>
+                    <option value="3">Furniture</option>
+                  </select>
+                  </div>
+              </div>
+              <div class="mb-3 row">
+                  <label for="inputDokumen" class="col-sm-2 col-form-label">Dokumen</label>
+                  <div class="col-sm-10">
+                    <b-form-file v-model="dokumen" id="file-default"></b-form-file>
+                  </div>
+              </div>
+              <div class="mb-3 row">
+                  <label for="inputPencatatan" class="col-sm-2 col-form-label">Pencatat</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputPencatatan" v-model="pencatat">
+                  </div>
+              </div>
+            </form>
+            <!-- eslint-disable -->
+            <template #modal-footer>
+                <b-button @click="simpan" variant="primary">Simpan</b-button>
+            </template>
+          </b-modal>
         </div>
       </div>
     </div>
-    <div class="row mt-3">
+    <div class="row">
       <table class="table">
         <thead class="table table-dark table-bordered border-white">
           <tr>
             <th>Kode Inventaris</th>
             <th>Tanggal Masuk</th>
+            <th>Barang</th>
             <th>Donatur</th>
+            <th>Ruang</th>
             <th>Divisi</th>
-            <th>Status Lokasi</th>
             <th>aksi</th>
           </tr>
         </thead>
@@ -125,9 +111,10 @@
           <tr>
             <th>1.</th>
             <th>13-09-2021</th>
-            <th>Ahmad Faizi</th>
+            <th>TV</th>
+            <th>PT. Kusuma Bangsa</th>
+            <th>Ruang Meeting</th>
             <th>CMC</th>
-            <th>Gedung Kelas</th>
             <th>
               <a href="/" class="btn btn-danger" role="button" data-bs-toggle="button">Delete</a>
               <a href="/" class="btn btn-primary" role="button" data-bs-toggle="button">Edit</a>
--- pages/inventaris/pengecekan.vue
+++ pages/inventaris/pengecekan.vue
@@ -1,60 +1,51 @@
 <template>
   <div class="container-fluid p-3">
-       <ol class="breadcrumb">
+    <nav aria-label="breadcrumb" class="mt-4">
+      <ol class="breadcrumb">
         <li class="breadcrumb-item" aria-current="page"><a href="/">Home</a></li>
-        <li class="breadcrumb-item" aria-current="page">Manajemen Inventaris</li>
-        <li class="breadcrumb-item active" aria-current="page">Pengecekan</li>
+        <li class="breadcrumb-item" aria-current="page">Manajemen Lokasi</li>
+        <li class="breadcrumb-item active" aria-current="page">Divisi</li>
       </ol>
+    </nav>
     <h4 class="ml-3"><strong>Data Pengecekan</strong></h4>
     <div class="row">
       <div class="ml-3 mt-4">
-        <button class="btn btn-sm btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#inputPengecekan">Tambah Pengecekan</button>
+        <b-button v-b-modal.modal-1 class="btn btn-sm" variant="primary">Tambah Pengecekan</b-button>
 
-        <div class="modal fade" id="inputPengecekan" tabindex="-1" aria-labelledby="inputPengecekanLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="inputPengecekanLabel">Tambah Pengecekan</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <b-modal id="modal-1" size='lg' ref="modal-pengecekan" title="Tambah Pengecekan">
+          <form action="" method="post" style="margin-bottom: 90px">
+              <div class="mb-3 row">
+                        <label for="tanggal" class="col-sm-2 col-form-label">Tanggal</label>
+                      <div class="col-sm-10">
+                        <b-form-datepicker id="tanggal" v-model="date" class="mb-2"></b-form-datepicker>
+                      </div>
               </div>
-              <div class="modal-body">
-                <form action="" method="post">
-                  <div class="mb-3 row">
-                            <label for="tanggal" class="col-sm-2 col-form-label">Tanggal</label>
-                          <div class="col-sm-10">
-                            <b-form-datepicker id="tanggal" v-model="date" class="mb-2"></b-form-datepicker>
-                          </div>
-                  </div>
-                  <div class="mb-3 row">
-                        <label for="kondisi" class="col-sm-2 col-form-label">Kondisi</label>
-                        <div class="col-sm-10">
-                        <select class="form-select" v-model="kondisi" aria-label="Default select example" id="kondisi">
-                          <option selected>Pilih Ruang</option>
-                          <option value="1">Baik</option>
-                          <option value="2">Cukup</option>
-                          <option value="2">Kurang Baik</option>
-                          <option value="2">Rusak</option>
-                        </select>
-                        </div>
-                  </div>
-                  <div class="mb-3 row">
-                    <label for="InputPengecek" class="col-sm-2 col-form-label">Pengecek</label>
+              <div class="mb-3 row">
+                    <label for="kondisi" class="col-sm-2 col-form-label">Kondisi</label>
                     <div class="col-sm-10">
-                      <input type="textarea" class="form-control" v-model="pengecekan" id="InputPengecek">
+                    <select class="form-select" v-model="kondisi" aria-label="Default select example" id="kondisi">
+                      <option selected>Pilih Ruang</option>
+                      <option value="1">Baik</option>
+                      <option value="2">Cukup</option>
+                      <option value="2">Kurang Baik</option>
+                      <option value="2">Rusak</option>
+                    </select>
                     </div>
-                  </div>
-                </form>
               </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save</button>
+              <div class="mb-3 row">
+                <label for="InputPengecek" class="col-sm-2 col-form-label">Pengecek</label>
+                <div class="col-sm-10">
+                  <input type="textarea" class="form-control" v-model="pengecekan" id="InputPengecek">
+                </div>
               </div>
-            </div>
-          </div>
-        </div>
+          </form>
+            <template #modal-footer>
+                <b-button @click="simpan" variant="primary">Simpan</b-button>
+            </template>
+        </b-modal>
       </div>
     </div>
-    <div class="row mt-3">
+    <div class="row">
       <table class="table">
         <thead class="table table-dark table-bordered border-white">
           <tr>
